a208c1646471e1a9e341b8ac2620937bb04fb4cb
mansoursaid001 <mansoursaid001@hotmail.com>
2015-10-07 14:52:21 +0200

    Fixing post model fillable attribute

--- app/Post.php
+++ app/Post.php
@@ -7,8 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $fillable = [
-        'body',
-        'vote_count'
+        'body'
     ];
 
     protected $table = 'posts';
