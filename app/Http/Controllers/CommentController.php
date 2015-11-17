6c144d018d59ba41862d5f07c35fb24ce1bfe4c3
mansoursaid001 <mansoursaid001@hotmail.com>
2015-11-17 21:39:18 +0200

    walla3 el donya

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,22 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Comment;
+use Auth;
+use Validator;
 use Illuminate\Http\Request;
 use App\Http\Requests;
-use App\Http\Controllers\Controller;
+use Response;
 
 class CommentController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
     /**
      * Show the form for creating a new resource.
      *
@@ -25,7 +18,7 @@ class CommentController extends Controller
      */
     public function create()
     {
-        //
+        return view('comments.create');
     }
 
     /**
@@ -36,50 +29,59 @@ class CommentController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(),[
+            'body' => 'required',
+            'post_id' => 'required'
+            ]);
+        if($validator->fails()){
+            return Response::make($validator->messages(), 400);
+        }
+        $comment = Auth::user()->comments()->create($request->all());
+        return view('comments.show', compact($comment));
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param Comment $comment
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Comment $comment)
     {
-        //
+        return view('comments.show', Auth::user()->comments()->findOrFail($comment));
     }
 
     /**
-     * Show the form for editing the specified resource.
+     * Show the form for editin g the specified resource.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param Comment $comment
+     * @return \Illuminate\View\View
      */
-    public function edit($id)
+    public function edit(Comment $comment)
     {
-        //
+        return view('comments.edit', compact($comment));
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \Illuminate\Http\Request $request
+     * @param Comment $comment
      * @return \Illuminate\Http\Response
+     * @internal param int $id
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Comment $comment)
     {
-        //
+
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param Comment $comment
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Comment $comment)
     {
         //
     }
